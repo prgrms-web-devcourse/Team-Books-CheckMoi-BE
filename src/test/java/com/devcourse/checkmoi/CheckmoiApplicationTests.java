4a77574b225d924956ed2b3a2bb9ee2b31cfdcb6
yongckim <zxcv9203@naver.com>
2022-07-26 10:40:14 +0900

    🎉 init : 프로젝트 초기 설정
    필요한 의존성 파일 추가

---
+++ src/test/java/com/devcourse/checkmoi/CheckmoiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devcourse.checkmoi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckmoiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
