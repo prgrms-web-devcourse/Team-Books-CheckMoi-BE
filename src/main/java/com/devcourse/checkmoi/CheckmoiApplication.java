9790b89be049129dea19a969ec293d7bf8e3def1
zxcv9203 <41960243+zxcv9203@users.noreply.github.com>
2022-07-26 11:00:22 +0900

    🎉 init : 프로젝트 초기 설정 (#8)
    * 🎉 init : 프로젝트 초기 설정
    
    필요한 의존성 파일 추가
    
    * 🔧 chore : .gitignore .DS_Store 추가
    
    MAC에서 Finder로 킬 시 생기는 해당 폴더 .gitignore 처리

---
+++ src/main/java/com/devcourse/checkmoi/CheckmoiApplication.java
@@ -0,0 +1,13 @@
+package com.devcourse.checkmoi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckmoiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CheckmoiApplication.class, args);
+    }
+
+}
