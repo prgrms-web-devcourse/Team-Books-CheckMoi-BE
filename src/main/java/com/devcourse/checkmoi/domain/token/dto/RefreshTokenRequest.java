e5f0af3cbc5132b3689ba143bfe2eb8abaf8f51c
kimziou77 <kimziou77@naver.com>
2022-07-27 15:11:31 +0900

    ♻️ refactor : dto record 타입 통일

--- src/main/java/com/devcourse/checkmoi/domain/token/dto/RefreshTokenRequest.java
+++ src/main/java/com/devcourse/checkmoi/domain/token/dto/RefreshTokenRequest.java
@@ -2,17 +2,9 @@ package com.devcourse.checkmoi.domain.token.dto;
 
 import javax.validation.constraints.NotBlank;
 
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-@Getter
-@AllArgsConstructor
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
-public class RefreshTokenRequest {
-
+public record RefreshTokenRequest(
     @NotBlank(message = "refreshToken은 비어있을 수 없습니다.")
-    private String refreshToken;
+    String refreshToken
+) {
 
 }
