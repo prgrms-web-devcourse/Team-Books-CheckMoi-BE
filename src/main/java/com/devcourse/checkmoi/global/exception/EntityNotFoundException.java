cede8aa259528af52ef785729094e9a45ca0ee92
ynoolee <53856184+ynoolee@users.noreply.github.com>
2022-07-27 18:18:28 +0900

    ✨ feat : api 응답에 대한 공동 객체 정의 및 BusinessException 정의 추가 (#17)
    * ✨ feat : api 응답에 대한 공동 객체 정의 및 BusinessException 정의 추가
    
    - 정상적인 상황에 대한 공동 응답객체인 SuccessResponse 를 정의
    - 예외 상황에 대한 공동 응답객체인 Error 와 ErrorField 를 정의
    - 예외 상황에 클라이언트에게 전달할 에러메시지를 관리하는 ErrorMessage 를 정의
    - 커스텀하게 예외를 관리하기 위해, 가장 상위 예외인 BusinessException 을 정의하고 그 하위 예외로 InvalidValue, EntityNotFoundException 을 정의
    - 글로벌한 예외를 처리하도록 GlobalExceptionHandler 의 초기 코드를 작성
    ---- 로그 형태 : [ERROR] 예외클래스이름 : 예외
    
    * ♻️ refactor : Collectors 의 정적 메소드들을 import static 하도록 변경한다
    
    - 간결함을 유지할 수 있다

---
+++ src/main/java/com/devcourse/checkmoi/global/exception/EntityNotFoundException.java
@@ -0,0 +1,12 @@
+package com.devcourse.checkmoi.global.exception;
+
+public class EntityNotFoundException extends BusinessException {
+
+    public EntityNotFoundException(String message) {
+        super(message, ErrorMessage.ENTITY_NOT_FOUND);
+    }
+
+    public EntityNotFoundException(String message, ErrorMessage errorMessage) {
+        super(message, errorMessage);
+    }
+}
