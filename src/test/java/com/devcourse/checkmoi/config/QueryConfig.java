a0ee57827540e8228d8e1f861b5d5faa965fc7e0
kimziou77 <kimziou77@naver.com>
2022-07-28 15:15:29 +0900

    🧪 test : 테스트 기본 템플릿 설정

---
+++ src/test/java/com/devcourse/checkmoi/config/QueryConfig.java
@@ -0,0 +1,22 @@
+package com.devcourse.checkmoi.config;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@TestConfiguration
+public class QueryConfig {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory() {
+        return new JPAQueryFactory(entityManager);
+    }
+
+}
