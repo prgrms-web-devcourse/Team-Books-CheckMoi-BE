8de1de5a9b3053bd98c790e1482e1e09cd182a80
kimyo <41179265+kimziou77@users.noreply.github.com>
2022-07-28 15:49:31 +0900

    🧪 test : 테스트 기본 템플릿 설정 (#22)

---
+++ src/test/java/com/devcourse/checkmoi/template/ApiTest.java
@@ -0,0 +1,59 @@
+package com.devcourse.checkmoi.template;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.removeHeaders;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+@ExtendWith(RestDocumentationExtension.class)
+public abstract class ApiTest {
+
+    protected MockMvc mockMvc;
+
+    @Autowired
+    protected ObjectMapper objectMapper;
+
+    protected RestDocumentationResultHandler restDocs;
+
+    @BeforeEach
+    void setUpMockMvc(WebApplicationContext context, RestDocumentationContextProvider provider) {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(context)
+            .addFilter(new CharacterEncodingFilter("UTF-8", true))
+            .apply(documentationConfiguration(provider))
+            .alwaysDo(print())
+            .build();
+    }
+
+    @BeforeEach
+    void setUpRestDocs(WebApplicationContext context) {
+        this.restDocs = document(
+            "{class-name}/{method-name}",
+            preprocessRequest(prettyPrint()),
+            preprocessResponse(removeHeaders("Transfer-Encoding",
+                "Date",
+                "Keep-Alive",
+                "Connection"
+            ), prettyPrint())
+        );
+    }
+
+    protected String toJson(Object object) throws JsonProcessingException {
+        return objectMapper.writeValueAsString(object);
+    }
+}
