7b6cbcca373d7408186976c3600f49e06295069a
kimziou77 <kimziou77@naver.com>
2022-07-29 21:04:55 +0900

    🎨 style : 코드컨벤션에 맞게 정렬

--- src/main/java/com/devcourse/checkmoi/domain/study/model/Study.java
+++ src/main/java/com/devcourse/checkmoi/domain/study/model/Study.java
@@ -2,7 +2,6 @@ package com.devcourse.checkmoi.domain.study.model;
 
 import static javax.persistence.GenerationType.IDENTITY;
 import static lombok.AccessLevel.PROTECTED;
-
 import com.devcourse.checkmoi.domain.book.model.Book;
 import com.devcourse.checkmoi.global.model.BaseEntity;
 import java.time.LocalDate;
@@ -20,18 +19,24 @@ public class Study extends BaseEntity {
     @Id
     @GeneratedValue(strategy = IDENTITY)
     private Long id;
-    private String name;
-    private String thumbnailUrl;
-    private String description;
-    private Integer maxParticipant;
 
+    private String name;
+
+    private String thumbnailUrl;
+
+    private String description;
+
+    private Integer maxParticipant;
+    
     @ManyToOne(fetch = FetchType.LAZY)
     private Book book;
 
     private LocalDate gatherStartDate;
+
     private LocalDate gatherEndDate;
 
     private LocalDate studyStartDate;
+
     private LocalDate studyEndDate;
 
 }
