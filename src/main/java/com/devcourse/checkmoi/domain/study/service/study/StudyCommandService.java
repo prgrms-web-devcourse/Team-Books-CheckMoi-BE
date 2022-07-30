e8a09d6b8a8fa469cd97fc4854175ac9a1b7fcfc
kimziou77 <kimziou77@naver.com>
2022-07-30 19:55:39 +0900

    ✨ feat : 스터디 수정 서비스 API 제작

--- src/main/java/com/devcourse/checkmoi/domain/study/service/study/StudyCommandService.java
+++ src/main/java/com/devcourse/checkmoi/domain/study/service/study/StudyCommandService.java
@@ -1,8 +1,11 @@
 package com.devcourse.checkmoi.domain.study.service.study;
 
 import com.devcourse.checkmoi.domain.study.dto.StudyRequest.Create;
+import com.devcourse.checkmoi.domain.study.dto.StudyRequest.Edit;
 
 public interface StudyCommandService {
 
     Long createStudy(Create request);
+
+    Long editStudyInfo(Long studyId, Edit request);
 }
--- src/main/java/com/devcourse/checkmoi/domain/study/service/study/StudyCommandServiceImpl.java
+++ src/main/java/com/devcourse/checkmoi/domain/study/service/study/StudyCommandServiceImpl.java
@@ -2,6 +2,9 @@ package com.devcourse.checkmoi.domain.study.service.study;
 
 import com.devcourse.checkmoi.domain.study.converter.StudyConverter;
 import com.devcourse.checkmoi.domain.study.dto.StudyRequest.Create;
+import com.devcourse.checkmoi.domain.study.dto.StudyRequest.Edit;
+import com.devcourse.checkmoi.domain.study.exception.StudyNotFoundException;
+import com.devcourse.checkmoi.domain.study.model.Study;
 import com.devcourse.checkmoi.domain.study.repository.study.StudyRepository;
 import javax.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
@@ -10,8 +13,7 @@ import org.springframework.stereotype.Service;
 @Service
 @Transactional
 @RequiredArgsConstructor
-public class StudyCommandServiceImpl implements
-    StudyCommandService {
+public class StudyCommandServiceImpl implements StudyCommandService {
 
     private final StudyConverter studyConverter;
 
@@ -22,6 +24,16 @@ public class StudyCommandServiceImpl implements
         return studyRepository
             .save(studyConverter.createToEntity(request))
             .getId();
+    }
+
+    @Override
+    public Long editStudyInfo(Long studyId, Edit request) {
+        Study study = studyRepository.findById(studyId)
+            .orElseThrow(StudyNotFoundException::new);
+        study.editName(request.name());
+        study.editThumbnail(request.thumbnail());
+        study.editDescription(request.description());
+        return study.getId();
     }
 
 }
