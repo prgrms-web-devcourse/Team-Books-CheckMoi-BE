69ca283f5cd851beb05a982c8371d3a58540d833
kimziou77 <kimziou77@naver.com>
2022-07-30 19:54:43 +0900

    ✨ feat : 스터디 수정 API 제작

--- src/main/java/com/devcourse/checkmoi/domain/study/api/StudyApi.java
+++ src/main/java/com/devcourse/checkmoi/domain/study/api/StudyApi.java
@@ -2,12 +2,15 @@ package com.devcourse.checkmoi.domain.study.api;
 
 import static com.devcourse.checkmoi.global.util.ApiUtil.generatedUri;
 import com.devcourse.checkmoi.domain.study.dto.StudyRequest.Create;
+import com.devcourse.checkmoi.domain.study.dto.StudyRequest.Edit;
 import com.devcourse.checkmoi.domain.study.service.study.StudyCommandService;
 import com.devcourse.checkmoi.global.model.SuccessResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,5 +30,12 @@ public class StudyApi {
         return ResponseEntity
             .created(generatedUri(studyId))
             .body(new SuccessResponse<>(studyId));
+    }
+
+    @PutMapping("/studies/{studyId}")
+    public ResponseEntity<SuccessResponse<Long>> editStudyInfo(
+        @PathVariable Long studyId, @RequestBody Edit request) {
+        return ResponseEntity.ok(
+            new SuccessResponse<>(studyCommandService.editStudyInfo(studyId, request)));
     }
 }
