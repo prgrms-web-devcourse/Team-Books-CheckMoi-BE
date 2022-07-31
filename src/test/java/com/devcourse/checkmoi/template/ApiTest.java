fa06c840701514dd758b1942ac20176aa1e803b8
kimziou77 <kimziou77@naver.com>
2022-07-31 21:24:50 +0900

    🔥 remove : 사용하지 않는 일부 API Test 메서드 제거

--- src/test/java/com/devcourse/checkmoi/template/ApiTest.java
+++ src/test/java/com/devcourse/checkmoi/template/ApiTest.java
@@ -1,11 +1,6 @@
 package com.devcourse.checkmoi.template;
 
-import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.removeHeaders;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -15,7 +10,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
-import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
@@ -29,8 +23,6 @@ public abstract class ApiTest {
     @Autowired
     protected ObjectMapper objectMapper;
 
-    protected RestDocumentationResultHandler restDocs;
-
     @BeforeEach
     void setUpMockMvc(WebApplicationContext context, RestDocumentationContextProvider provider) {
         this.mockMvc = MockMvcBuilders.webAppContextSetup(context)
@@ -39,19 +31,6 @@ public abstract class ApiTest {
             .apply(springSecurity())
             .alwaysDo(print())
             .build();
-    }
-
-    @BeforeEach
-    void setUpRestDocs(WebApplicationContext context) {
-        this.restDocs = document(
-            "{class-name}/{method-name}",
-            preprocessRequest(prettyPrint()),
-            preprocessResponse(removeHeaders("Transfer-Encoding",
-                "Date",
-                "Keep-Alive",
-                "Connection"
-            ), prettyPrint())
-        );
     }
 
     protected String toJson(Object object) throws JsonProcessingException {
