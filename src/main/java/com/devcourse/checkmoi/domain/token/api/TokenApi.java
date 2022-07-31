0265412f62307772297d3542cec93fbd72bbcfa1
kimziou77 <kimziou77@naver.com>
2022-07-31 21:26:20 +0900

    🧪 test : Token API 테스트 작성

--- src/main/java/com/devcourse/checkmoi/domain/token/api/TokenApi.java
+++ src/main/java/com/devcourse/checkmoi/domain/token/api/TokenApi.java
@@ -7,10 +7,10 @@ import com.devcourse.checkmoi.global.model.SuccessResponse;
 import com.devcourse.checkmoi.global.security.jwt.JwtAuthentication;
 import com.devcourse.checkmoi.global.security.util.AuthorizationExtractor;
 import javax.servlet.http.HttpServletRequest;
-import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -27,7 +27,7 @@ public class TokenApi {
     @PostMapping("/tokens")
     public ResponseEntity<SuccessResponse<AccessToken>> refreshAccessToken(
         HttpServletRequest httpServletRequest,
-        @Valid @RequestBody TokenRequest.RefreshToken refreshToken
+        @Validated @RequestBody TokenRequest.RefreshToken refreshToken
     ) {
         String accessToken = AuthorizationExtractor.extract(httpServletRequest);
         return ResponseEntity.ok(
@@ -36,9 +36,10 @@ public class TokenApi {
 
     @DeleteMapping("/logout")
     public ResponseEntity<Void> deleteRefreshToken(
-        @AuthenticationPrincipal JwtAuthentication user) {
+        @AuthenticationPrincipal JwtAuthentication user
+    ) {
         tokenService.deleteTokenByUserId(user.id());
         return ResponseEntity.noContent().build();
     }
-    
+
 }
