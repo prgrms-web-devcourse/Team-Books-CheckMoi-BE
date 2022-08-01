d7b66545fde17cefe6eb529e80ac89c8b9be1b4b
ynoolee <heerarea129@gmail.com>
2022-08-01 23:27:52 +0900

    ✨ feat : S3 프로퍼티즈 설정 (#40)

--- src/main/java/com/devcourse/checkmoi/global/config/WebConfig.java
+++ src/main/java/com/devcourse/checkmoi/global/config/WebConfig.java
@@ -1,7 +1,13 @@
 package com.devcourse.checkmoi.global.config;
 
-import static org.springframework.http.HttpHeaders.*;
-
+import static org.springframework.http.HttpHeaders.LOCATION;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.devcourse.checkmoi.domain.file.service.S3Upload;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -17,5 +23,23 @@ public class WebConfig implements WebMvcConfigurer {
             .allowedMethods(ALLOWED_METHOD_NAMES.split(","))
             .exposedHeaders(LOCATION);
     }
-    
+
+    @Bean
+    public AmazonS3 amazonS3Client(AwsConfigProperties awsConfigure) {
+        return AmazonS3ClientBuilder.standard()
+            .withRegion(Regions.fromName(awsConfigure.getRegion()))
+            .withCredentials(
+                new AWSStaticCredentialsProvider(
+                    new BasicAWSCredentials(
+                        awsConfigure.getAccessKey(),
+                        awsConfigure.getSecretKey())
+                )
+            )
+            .build();
+    }
+
+    @Bean
+    public S3Upload s3Upload(AmazonS3 amazonS3, AwsConfigProperties awsProperties) {
+        return new S3Upload(amazonS3, awsProperties.getUrl(), awsProperties.getBucketName());
+    }
 }
