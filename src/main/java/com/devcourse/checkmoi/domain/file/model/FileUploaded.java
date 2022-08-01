fd58ef789c129c9fd206432aebf1a14c4bcd0e2e
ynoolee <heerarea129@gmail.com>
2022-08-02 01:07:54 +0900

    ♻️ refactor : 엔티티 생성전략 추가 (#40)
    - FileUploaded 에 엔티티 생성전략을 추가한다

--- src/main/java/com/devcourse/checkmoi/domain/file/model/FileUploaded.java
+++ src/main/java/com/devcourse/checkmoi/domain/file/model/FileUploaded.java
@@ -5,6 +5,7 @@ import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -15,7 +16,7 @@ import lombok.NoArgsConstructor;
 public class FileUploaded extends BaseEntity {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String extension;
