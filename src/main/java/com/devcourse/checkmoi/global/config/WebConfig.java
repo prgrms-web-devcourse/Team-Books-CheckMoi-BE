b5b6a1fcead26dcdce663f19757d9391a098f87c
kimziou77 <kimziou77@naver.com>
2022-08-05 16:37:07 +0900

    🔧 chore : 사용 포트 추가

--- src/main/java/com/devcourse/checkmoi/global/config/WebConfig.java
+++ src/main/java/com/devcourse/checkmoi/global/config/WebConfig.java
@@ -19,7 +19,11 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry
             .addMapping("/**")
-            .allowedOrigins("http://localhost:3000", "https://checkmoi.vercel.app")
+            .allowedOrigins(
+                "http://localhost:3000",
+                "https://checkmoi.vercel.app",
+                "http://localhost:6006"
+            )
             .allowedMethods("*")
             .allowCredentials(true)
             .allowedHeaders("*")
