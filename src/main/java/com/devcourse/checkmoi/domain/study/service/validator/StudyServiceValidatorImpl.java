60ec86172b32eefad8c775c06382fd639ad10c51
kimziou77 <kimziou77@naver.com>
2022-08-05 12:04:40 +0900

    ✨ feat : service - validator 책임 분리

---
+++ src/main/java/com/devcourse/checkmoi/domain/study/service/validator/StudyServiceValidatorImpl.java
@@ -0,0 +1,33 @@
+package com.devcourse.checkmoi.domain.study.service.validator;
+
+import static com.devcourse.checkmoi.global.exception.ErrorMessage.ACCESS_DENIED;
+import static com.devcourse.checkmoi.global.exception.ErrorMessage.STUDY_JOIN_REQUEST_DUPLICATE;
+import com.devcourse.checkmoi.domain.study.exception.DuplicateStudyJoinRequestException;
+import com.devcourse.checkmoi.domain.study.exception.NotStudyOwnerException;
+import com.devcourse.checkmoi.domain.study.exception.StudyNotFoundException;
+import com.devcourse.checkmoi.domain.study.model.StudyMember;
+import com.devcourse.checkmoi.domain.study.model.StudyMemberStatus;
+import org.springframework.stereotype.Component;
+
+@Component
+public class StudyServiceValidatorImpl implements StudyServiceValidator {
+
+    public void validateExistStudy(boolean existStudy) {
+        if (!existStudy) {
+            throw new StudyNotFoundException();
+        }
+    }
+
+    public void validateStudyOwner(Long userId, Long studyOwnerId, String message) {
+        if (!studyOwnerId.equals(userId)) {
+            throw new NotStudyOwnerException(message, ACCESS_DENIED);
+        }
+    }
+
+    public void validateDuplicateStudyMemberRequest(StudyMember studyMember) {
+        if (studyMember.getStatus() != StudyMemberStatus.DENIED) {
+            throw new DuplicateStudyJoinRequestException(STUDY_JOIN_REQUEST_DUPLICATE);
+        }
+    }
+
+}
