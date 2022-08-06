af75df2ab5965a0d40669bbd79523f70598b965a
kimziou77 <kimziou77@naver.com>
2022-08-06 20:52:19 +0900

    ♻️ refactor : 게시글도메인에서 Custom레포지토리를 사용할 수 있도록 변경

--- src/main/java/com/devcourse/checkmoi/domain/post/repository/PostRepository.java
+++ src/main/java/com/devcourse/checkmoi/domain/post/repository/PostRepository.java
@@ -3,6 +3,6 @@ package com.devcourse.checkmoi.domain.post.repository;
 import com.devcourse.checkmoi.domain.post.model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PostRepository extends JpaRepository<Post, Long> {
+public interface PostRepository extends JpaRepository<Post, Long>, CustomPostRepository {
 
 }
