808474b89a0c8e57c99d1731e6b9ae42352498e8
kimyo <41179265+kimziou77@users.noreply.github.com>
2022-08-07 23:34:47 +0900

    🔧 chore : yml을 통해 시큐리티 설정 정보 관리 (#144)

--- src/main/java/com/devcourse/checkmoi/global/config/SecurityConfig.java
+++ src/main/java/com/devcourse/checkmoi/global/config/SecurityConfig.java
@@ -1,10 +1,19 @@
-package com.devcourse.checkmoi.global.security.config;
+package com.devcourse.checkmoi.global.config;
 
+import static org.springframework.http.HttpMethod.DELETE;
+import static org.springframework.http.HttpMethod.GET;
+import static org.springframework.http.HttpMethod.OPTIONS;
+import static org.springframework.http.HttpMethod.PATCH;
+import static org.springframework.http.HttpMethod.POST;
+import static org.springframework.http.HttpMethod.PUT;
+import com.devcourse.checkmoi.global.config.properties.SecurityConfigProperties;
 import com.devcourse.checkmoi.global.security.handler.ExceptionHandlerFilter;
 import com.devcourse.checkmoi.global.security.handler.JwtAuthenticationEntryPoint;
 import com.devcourse.checkmoi.global.security.handler.OAuthAuthenticationSuccessHandler;
 import com.devcourse.checkmoi.global.security.jwt.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -15,8 +24,9 @@ import org.springframework.security.oauth2.client.web.OAuth2AuthorizationRequest
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
 @RequiredArgsConstructor
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
+@EnableConfigurationProperties(SecurityConfigProperties.class)
 public class SecurityConfig {
 
     private final OAuthAuthenticationSuccessHandler oAuth2SuccessHandler;
@@ -27,10 +37,18 @@ public class SecurityConfig {
 
     private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
 
+    private final SecurityConfigProperties securityConfigProperties;
+
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
         return web -> web.ignoring()
-            .antMatchers("/static/**", "/favicon.ico", "/api/tokens/**");
+            .antMatchers(securityConfigProperties.patterns().ignoring().get("ALL"))
+            .antMatchers(GET, securityConfigProperties.patterns().ignoring().get("GET"))
+            .antMatchers(PUT, securityConfigProperties.patterns().ignoring().get("PUT"))
+            .antMatchers(POST, securityConfigProperties.patterns().ignoring().get("POST"))
+            .antMatchers(PATCH, securityConfigProperties.patterns().ignoring().get("PATCH"))
+            .antMatchers(DELETE, securityConfigProperties.patterns().ignoring().get("DELETE"))
+            .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
     }
 
     @Bean
@@ -42,7 +60,21 @@ public class SecurityConfig {
 
             // permission
             .authorizeHttpRequests()
-            .anyRequest().permitAll()
+            .antMatchers(
+                securityConfigProperties.patterns().permitAll().get("ALL")).permitAll()
+            .antMatchers(GET,
+                securityConfigProperties.patterns().permitAll().get("GET")).permitAll()
+            .antMatchers(PUT,
+                securityConfigProperties.patterns().permitAll().get("PUT")).permitAll()
+            .antMatchers(POST,
+                securityConfigProperties.patterns().permitAll().get("POST")).permitAll()
+            .antMatchers(PATCH,
+                securityConfigProperties.patterns().permitAll().get("PATCH")).permitAll()
+            .antMatchers(DELETE,
+                securityConfigProperties.patterns().permitAll().get("DELETE")).permitAll()
+            .antMatchers(OPTIONS,
+                securityConfigProperties.patterns().permitAll().get("OPTIONS")).permitAll()
+            .anyRequest().authenticated()
             .and()
 
             // oauth
