403688423d916db80997a2ca9681578f69b99d71
kimziou77 <kimziou77@naver.com>
2022-08-07 15:15:23 +0900

    ♻️ refactor : Integration Test용 레포지토리 추가

--- src/test/java/com/devcourse/checkmoi/domain/comment/service/CommentQueryServiceImplTest.java
+++ src/test/java/com/devcourse/checkmoi/domain/comment/service/CommentQueryServiceImplTest.java
@@ -13,64 +13,28 @@ import static com.devcourse.checkmoi.util.EntityGeneratorUtil.makeStudyMember;
 import static com.devcourse.checkmoi.util.EntityGeneratorUtil.makeUser;
 import static org.assertj.core.api.Assertions.assertThat;
 import com.devcourse.checkmoi.domain.book.model.Book;
-import com.devcourse.checkmoi.domain.book.repository.BookRepository;
 import com.devcourse.checkmoi.domain.comment.converter.CommentConverter;
 import com.devcourse.checkmoi.domain.comment.dto.CommentRequest.Search;
 import com.devcourse.checkmoi.domain.comment.dto.CommentResponse.CommentInfo;
-import com.devcourse.checkmoi.domain.comment.repository.CommentRepository;
 import com.devcourse.checkmoi.domain.post.model.Post;
-import com.devcourse.checkmoi.domain.post.repository.PostRepository;
 import com.devcourse.checkmoi.domain.study.model.Study;
-import com.devcourse.checkmoi.domain.study.repository.StudyMemberRepository;
-import com.devcourse.checkmoi.domain.study.repository.StudyRepository;
 import com.devcourse.checkmoi.domain.user.model.User;
-import com.devcourse.checkmoi.domain.user.repository.UserRepository;
+import com.devcourse.checkmoi.template.IntegrationTest;
 import java.util.List;
 import java.util.stream.Stream;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class CommentQueryServiceImplTest {
+class CommentQueryServiceImplTest extends IntegrationTest {
 
     @Autowired
     private CommentQueryServiceImpl commentQueryService;
 
     @Autowired
-    private CommentRepository commentRepository;
-
-    @Autowired
-    private UserRepository userRepository;
-
-    @Autowired
-    private StudyRepository studyRepository;
-
-    @Autowired
-    private BookRepository bookRepository;
-
-    @Autowired
-    private StudyMemberRepository studyMemberRepository;
-
-    @Autowired
-    private PostRepository postRepository;
-
-    @Autowired
     private CommentConverter commentConverter;
-
-    @AfterEach
-    void cleanUp() {
-        commentRepository.deleteAllInBatch();
-        postRepository.deleteAllInBatch();
-        studyMemberRepository.deleteAllInBatch();
-        studyRepository.deleteAllInBatch();
-        bookRepository.deleteAllInBatch();
-        userRepository.deleteAllInBatch();
-    }
 
     @Nested
     @DisplayName("작성된 글에 대한 댓글 목록 조회 #130")
--- src/test/java/com/devcourse/checkmoi/template/IntegrationTest.java
+++ src/test/java/com/devcourse/checkmoi/template/IntegrationTest.java
@@ -6,12 +6,19 @@ import static org.springframework.restdocs.headers.HeaderDocumentation.requestHe
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import com.devcourse.checkmoi.domain.book.repository.BookRepository;
+import com.devcourse.checkmoi.domain.comment.repository.CommentRepository;
+import com.devcourse.checkmoi.domain.post.repository.PostRepository;
+import com.devcourse.checkmoi.domain.study.repository.StudyMemberRepository;
+import com.devcourse.checkmoi.domain.study.repository.StudyRepository;
 import com.devcourse.checkmoi.domain.token.dto.TokenResponse.TokenWithUserInfo;
+import com.devcourse.checkmoi.domain.user.repository.UserRepository;
 import com.devcourse.checkmoi.global.security.oauth.OAuthService;
 import com.devcourse.checkmoi.global.security.oauth.UserProfile;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.UUID;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,7 +41,25 @@ public abstract class IntegrationTest {
     protected ObjectMapper objectMapper;
 
     @Autowired
-    private OAuthService oAuthService;
+    protected OAuthService oAuthService;
+
+    @Autowired
+    protected CommentRepository commentRepository;
+
+    @Autowired
+    protected UserRepository userRepository;
+
+    @Autowired
+    protected StudyRepository studyRepository;
+
+    @Autowired
+    protected BookRepository bookRepository;
+
+    @Autowired
+    protected StudyMemberRepository studyMemberRepository;
+
+    @Autowired
+    protected PostRepository postRepository;
 
     private static UserProfile createUserProfile(String name) {
         return UserProfile.builder()
@@ -68,5 +93,15 @@ public abstract class IntegrationTest {
 
     protected TokenWithUserInfo getTokenWithUserInfo() {
         return oAuthService.register(createUserProfile(UUID.randomUUID().toString().substring(19)));
+    }
+
+    @AfterEach
+    void cleanUp() {
+        commentRepository.deleteAllInBatch();
+        postRepository.deleteAllInBatch();
+        studyMemberRepository.deleteAllInBatch();
+        studyRepository.deleteAllInBatch();
+        bookRepository.deleteAllInBatch();
+        userRepository.deleteAllInBatch();
     }
 }
