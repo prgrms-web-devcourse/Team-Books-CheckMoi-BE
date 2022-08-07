5b1649f8d5775e999936fd28166ffab730f95b4c
kimziou77 <kimziou77@naver.com>
2022-08-07 09:18:45 +0900

    ✨ feat : 댓글 DTO 컨버터 제작

---
+++ src/main/java/com/devcourse/checkmoi/domain/comment/converter/CommentConverter.java
@@ -0,0 +1,25 @@
+package com.devcourse.checkmoi.domain.comment.converter;
+
+import com.devcourse.checkmoi.domain.comment.dto.CommentResponse.CommentInfo;
+import com.devcourse.checkmoi.domain.comment.model.Comment;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CommentConverter {
+
+    public CommentInfo commentToInfo(Comment comment) {
+        return CommentInfo.builder()
+            .id(comment.getId())
+            .userId(comment.getUser().getId())
+            .content(comment.getContent())
+            .createdAt(convertLocalDate(comment.getCreatedAt()))
+            .updatedAt(convertLocalDate(comment.getUpdatedAt()))
+            .build();
+    }
+
+    private LocalDate convertLocalDate(LocalDateTime time) {
+        return time == null ? LocalDate.now() : time.toLocalDate();
+    }
+}
