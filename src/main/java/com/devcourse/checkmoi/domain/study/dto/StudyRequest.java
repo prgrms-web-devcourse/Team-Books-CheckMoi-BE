0c349a7baba432b790de74bb64369f05ab4dd852
kimziou77 <kimziou77@naver.com>
2022-08-09 19:56:35 +0900

    ✨ feat : 스터디 검색 Dto 제작

--- src/main/java/com/devcourse/checkmoi/domain/study/dto/StudyRequest.java
+++ src/main/java/com/devcourse/checkmoi/domain/study/dto/StudyRequest.java
@@ -3,10 +3,26 @@ package com.devcourse.checkmoi.domain.study.dto;
 import com.devcourse.checkmoi.domain.study.dto.StudyRequest.Audit;
 import com.devcourse.checkmoi.domain.study.dto.StudyRequest.Create;
 import com.devcourse.checkmoi.domain.study.dto.StudyRequest.Edit;
+import com.devcourse.checkmoi.domain.study.dto.StudyRequest.Search;
 import java.time.LocalDate;
 import lombok.Builder;
+import org.springframework.lang.Nullable;
 
-public sealed interface StudyRequest permits Create, Edit, Audit {
+public sealed interface StudyRequest permits Create, Edit, Audit, Search {
+
+    record Search(
+        @Nullable Long userId,
+        @Nullable Long studyId,
+        @Nullable Long bookId,
+        @Nullable String memberStatus,
+        @Nullable String studyStatus
+    ) implements StudyRequest {
+
+        @Builder
+        public Search {
+        }
+    }
+
 
     record Create(
         Long bookId,
