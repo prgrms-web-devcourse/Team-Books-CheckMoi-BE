85ab897cce6e807253a1048236823a6827350970
ynoolee <heerarea129@gmail.com>
2022-08-09 20:41:43 +0900

    ♻️ refactor : UserNotFound 예외 메시지 설정 (#154)

--- src/main/java/com/devcourse/checkmoi/domain/user/exception/UserNotFoundException.java
+++ src/main/java/com/devcourse/checkmoi/domain/user/exception/UserNotFoundException.java
@@ -2,13 +2,12 @@ package com.devcourse.checkmoi.domain.user.exception;
 
 
 import com.devcourse.checkmoi.global.exception.EntityNotFoundException;
+import com.devcourse.checkmoi.global.exception.error.ErrorMessage;
 
 public class UserNotFoundException extends EntityNotFoundException {
 
-    private static final String MESSAGE = "존재하지 않는 유저입니다";
-
     public UserNotFoundException() {
-        super(MESSAGE);
+        super(ErrorMessage.USER_NOT_FOUND);
     }
 
 }
