a9722da26b8d5fb7b524e05a279c917cc762f743
ynoolee <53856184+ynoolee@users.noreply.github.com>
2022-08-10 15:26:04 +0900

    ✨ feat : 스터디에 모집완료 상태 추가 에 따른 상태 변경 제약 로직 수정 및 테스트 (#168) (#169)
    * ✨ feat : 스터디에 모집완료 상태 추가 에 따른 상태 변경 제약 로직 수정 및 테스트 (#168)
    
    - 스터디 상태에 "모집완료" 가 추가됨에 따라, 상태 변경에 대한 제약 로직을 수정하고 이를 테스트함
    
    * 🧪 test : 스터디 편집 테스트에 상태 변경 성공 테스트 추가 (#168)

--- src/main/java/com/devcourse/checkmoi/domain/study/model/StudyStatus.java
+++ src/main/java/com/devcourse/checkmoi/domain/study/model/StudyStatus.java
@@ -1,14 +1,17 @@
 package com.devcourse.checkmoi.domain.study.model;
 
+import java.util.Collections;
 import java.util.Set;
 
 public enum StudyStatus {
     RECRUITING(
-        Set.of(NextStatus.IN_PROGRESS, NextStatus.RECRUITING)),
+        Set.of(NextStatus.IN_PROGRESS, NextStatus.RECRUITING_FINISHED)),
+    RECRUITING_FINISHED(
+        Set.of(NextStatus.IN_PROGRESS)),
     IN_PROGRESS(
-        Set.of(NextStatus.RECRUITING, NextStatus.IN_PROGRESS, NextStatus.FINISHED)),
+        Set.of(NextStatus.FINISHED)),
     FINISHED(
-        Set.of(NextStatus.FINISHED));
+        Collections.emptySet());
 
     private final Set<NextStatus> allowedNextStatus;
 
@@ -16,12 +19,23 @@ public enum StudyStatus {
         this.allowedNextStatus = allowedNextStatus;
     }
 
-    public boolean isAllowedNextStatus(StudyStatus status) {
-        return this.allowedNextStatus.contains(NextStatus.of(status));
+    public boolean isAllowedToChangeStatus(StudyStatus status) {
+        return this.isUnchanged(status) ||
+            this.isAllowedNextStatus(status);
+    }
+
+    private boolean isUnchanged(StudyStatus status) {
+        return this == status;
+    }
+
+    private boolean isAllowedNextStatus(StudyStatus status) {
+        return !this.allowedNextStatus.isEmpty() &&
+            this.allowedNextStatus.contains(NextStatus.of(status));
     }
 
     private enum NextStatus {
         RECRUITING,
+        RECRUITING_FINISHED,
         IN_PROGRESS,
         FINISHED;
 
