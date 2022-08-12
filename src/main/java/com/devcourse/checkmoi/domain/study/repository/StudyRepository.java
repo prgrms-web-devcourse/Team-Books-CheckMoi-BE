f45d992da08c1d0842bf5aa7a3f76f8d2820e7fe
kimziou77 <kimziou77@naver.com>
2022-08-12 15:25:12 +0900

    ♻️ refactor : 간단한 쿼리 JPQL로 변경

--- src/main/java/com/devcourse/checkmoi/domain/study/repository/StudyRepository.java
+++ src/main/java/com/devcourse/checkmoi/domain/study/repository/StudyRepository.java
@@ -2,8 +2,12 @@ package com.devcourse.checkmoi.domain.study.repository;
 
 import com.devcourse.checkmoi.domain.study.model.Study;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 
 public interface StudyRepository extends JpaRepository<Study, Long>, CustomStudyRepository {
 
+    @Query("select sm.user.id from StudyMember sm "
+        + "where sm.study.id = :studyId and sm.status = 'OWNED'")
+    Long findStudyOwner(Long studyId);
 }
