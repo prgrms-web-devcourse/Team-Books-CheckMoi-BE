898439a1e531cd85d65e2d218e4ad99d7496b24a
ynoolee <heerarea129@gmail.com>
2022-08-13 14:51:46 +0900

    ✨ feat :  이미지 용량 초과 예외메시지 추가 (#173)
    - 이미지 업로드 시 이미지 용량에 대한 예외메시지를 던지도록 하였음

--- src/main/java/com/devcourse/checkmoi/global/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/devcourse/checkmoi/global/exception/handler/GlobalExceptionHandler.java
@@ -4,11 +4,13 @@ import com.devcourse.checkmoi.global.exception.BusinessException;
 import com.devcourse.checkmoi.global.exception.error.ErrorMessage;
 import com.devcourse.checkmoi.global.exception.error.ErrorResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.validation.BindException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.multipart.MultipartException;
 
 @Slf4j
 @RestControllerAdvice
@@ -53,6 +55,16 @@ public class GlobalExceptionHandler {
         ErrorResponse response = ErrorResponse.of(e);
 
         return ResponseEntity.badRequest().body(response);
+    }
+
+    @ExceptionHandler(MultipartException.class)
+    protected ResponseEntity<ErrorResponse> handleFileSizeException(MultipartException e) {
+        log.error("{}", e.getMessage());
+
+        ErrorResponse response = ErrorResponse.of(ErrorMessage.FILE_SIZE_EXCEED);
+
+        return ResponseEntity.status(HttpStatus.PAYLOAD_TOO_LARGE)
+            .body(response);
     }
 
 }
