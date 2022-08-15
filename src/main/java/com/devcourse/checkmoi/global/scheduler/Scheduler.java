8a8fd9d3563e85af719e56050ed18c65f2626c69
ynoolee <53856184+ynoolee@users.noreply.github.com>
2022-08-15 16:14:59 +0900

    ✨ feat : 스케줄러 동작 api 추가 (#252) (#253)
    * ✨ feat : 스케줄러 동작 api 추가 (#252)
    
    - 스케줄러의 동작을 api 를 통해 호출할 수 있도록 추가
    
    * ♻️ refactor : api 에 스케줄러 주입 방식으로 변경 (#252)
    
    - 스케줄러 테스트 추가
    - jacoco 테스트 커버리지, 소나클라우드에서 ScheduleApi 는 제외
    
    * ♻️ refactor : isEmpty 로 변경 (#252)

--- src/main/java/com/devcourse/checkmoi/global/scheduler/Scheduler.java
+++ src/main/java/com/devcourse/checkmoi/global/scheduler/Scheduler.java
@@ -18,8 +18,8 @@ public class Scheduler {
         this.studyManager = studyManager;
     }
 
-    @Scheduled(cron = "0 30 * * * *")
-    void changeStudyAsInProgress() {
+    @Scheduled(cron = "0 0 * * * *")
+    public void changeStudyAsInProgress() {
         log.info(message + " 스터디 상태 변경 작업 (-> 진행 중 ) 시작");
 
         try {
@@ -31,14 +31,14 @@ public class Scheduler {
         log.info(message + " 스터디 상태 변경 작업 완료!!");
     }
 
-    @Scheduled(cron = "0 30 * * * *")
-    void changeStudyAsFinished() {
+    @Scheduled(cron = "0 0 * * * *")
+    public void changeStudyAsFinished() {
         log.info(message + " 스터디 상태 변경 작업 (-> 진행 완료 ) 시작");
 
         try {
             completeStudies();
         } catch (Exception e) {
-            log.error(message + "[ERROR] : 스터디를 진행중 상태로 변경하는 스케줄링 작업 실패", e);
+            log.error(message + "[ERROR] : 스터디를 진행완료 상태로 변경하는 스케줄링 작업 실패", e);
         }
 
         log.info(message + " 스터디 상태 변경 작업 완료!!");
@@ -46,15 +46,33 @@ public class Scheduler {
 
     private void progressStudies() {
         studyManager.getAllStudiesToBeProcessed(StudyStatus.IN_PROGRESS).studies()
-            .forEach(studyId ->
-                studyManager.updateStudyWithMembers(studyId, StudyStatus.IN_PROGRESS,
-                    StudyMemberStatus.DENIED));
+            .forEach(studyId -> this.updateStudyWithMembers(studyId, StudyStatus.IN_PROGRESS,
+                StudyMemberStatus.DENIED));
     }
 
     private void completeStudies() {
         studyManager.getAllStudiesToBeProcessed(StudyStatus.FINISHED).studies()
-            .forEach(studyId ->
-                studyManager.updateStudy(studyId, StudyStatus.FINISHED));
+            .forEach(studyId -> this.updateStudy(studyId, StudyStatus.FINISHED));
+    }
+
+    private void updateStudyWithMembers(Long studyId, StudyStatus studyStatus,
+        StudyMemberStatus memberStatus) {
+
+        try {
+            studyManager.updateStudyWithMembers(studyId, studyStatus, memberStatus);
+        } catch (Exception e) {
+            log.error(
+                message + "[ERROR] : 스터디를 진행중 상태로 변경하는 스케줄링 작업 실패 - 실패한 studyId : " + studyId);
+        }
+    }
+
+    private void updateStudy(Long studyId, StudyStatus studyStatus) {
+        try {
+            studyManager.updateStudy(studyId, studyStatus);
+        } catch (Exception e) {
+            log.error(
+                message + "[ERROR] : 스터디를 진행완료 상태로 변경하는 스케줄링 작업 실패 - 실패한 studyId : " + studyId);
+        }
     }
 
 }
